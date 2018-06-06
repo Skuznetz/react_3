8fc2e4ec589b85c3adeb3320dba5829a9864887f
Skuznetz <s.kuznetzov@mail.ru>
2018-06-06 21:14:41 +0400

    пишем классы и аттрибуты

--- src/Tweet.js
+++ src/Tweet.js
@@ -11,9 +11,41 @@ class Tweet extends Component{
       likes
   } = this.props;
     return (
-      <div>
-        
+      <div className="tweet">
+      <img className="tweet-avatar" src={avatar} />
+
+      <div className="tweet-body">
+          <a
+              className="tweet-author"
+              href={`https://twitter.com/${author}`}
+              target="blank"
+          >
+              @{author}
+          </a>
+
+          <p className="tweet-text">
+              {text}
+          </p>
+
+          {
+              image &&
+              <img className="tweet-image" src={image} />
+          }
+
+          <div className="tweet-stats">
+              <div className="tweet-retweets">
+                  <i className="tweet-stat-icon fa fa-retweet" />
+                  {retweets || null}
+              </div>
+              <div className="tweet-likes">
+                  <i className="tweet-stat-icon fa fa-heart" />
+                  {likes || null}
+              </div>
+          </div>
       </div>
+  </div>
+        
+      
     )
   }
 }
