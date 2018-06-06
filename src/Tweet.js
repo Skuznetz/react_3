a62b4e3256b9ac6949b6ebb6a610f4bc66e7c1f8
Skuznetz <s.kuznetzov@mail.ru>
2018-06-06 21:02:45 +0400

    создадим компонент Tweet

--- src/Tweet.js
+++ src/Tweet.js
@@ -1,1 +1,19 @@
 import React, { Component } from 'react';
+
+class Tweet extends Component{
+  render(){
+    const {
+      avatar,
+      author,
+      text,
+      image,
+      retweets,
+      likes
+  } = this.props;
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
