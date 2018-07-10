5410f0a4c4ca5d46dd50260d50eb85ffde7696f9
Skuznetz <s.kuznetzov@mail.ru>
2018-07-10 21:19:19 +0400

    изменим компонент

--- src/Tweet.js
+++ src/Tweet.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Tweet extends Component{
+class Tweet extends React.Component{
   render(){
     const {
       avatar,
